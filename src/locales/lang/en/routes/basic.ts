df62adffb0ca93d75a1de02e5174444f1101d552
uncle-light <chenhongguangs@gmail.com>
2022-07-15 23:28:48 +0800

    docs: deploy config

--- src/locales/lang/en/routes/basic.ts
+++ src/locales/lang/en/routes/basic.ts
@@ -2,8 +2,9 @@ export default {
   login: "Login",
   dashboard: "front pag",
   home: "workbench",
-  lessonPlan: "lessonPlan",
-  classification: "lessonPlanClassification",
-  lesson: "lesson",
-  lessonList: "lessonList",
+  test1: "test1",
+  test11: "test11",
+  test12: "test12",
+  test2: "test2",
+  test21: "test21",
 };
